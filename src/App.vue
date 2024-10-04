19f3bb9dd4fe76844db17339b88ba1228d876895
billClinton048 <clintonkaranja46@gmail.com>
2024-10-04 21:57:43 +0300

    message

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <SignIn />
+
+  </div>
+</template>
+
+<script>
+import SignIn from './components/SignIn.vue'
+  export default {
+    name: 'App',
+    components: {
+      SignIn
+    }
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/components/SignIn.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <img class="logo" src="../assets/logos.png" alt="" />
+    <h1>SignUp</h1>
+    <div class="signUp">
+        <input type="text" placeholder="Enter Name">
+        <input type="text" placeholder="Enter Email">
+        <input type="text" placeholder="Enter Password">
+        <button>Sign UP</button>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
+
+<style>
+.logo {
+  width: 100px;
+}
+.signUp input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+    }
+.signUp button{
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: white;
+    cursor: pointer;
+
+}
+</style>
